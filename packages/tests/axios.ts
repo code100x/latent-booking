929cf2ba7833b9c487062768f2aaa11a73f70c04
Vardaan Aggarwal <98269336+VardaanAggarwal@users.noreply.github.com>
2025-01-17 01:02:29 +0530

    fix: axios tyepscript types

--- packages/tests/axios.ts
+++ packages/tests/axios.ts
@@ -1,35 +1,35 @@
-import axios2 from "axios"
+import axios2 from "axios";
 export const axios = {
-    async post<T = any, R = axios2.AxiosResponse<T>, D = any>(url: string, data?: D, config?: axios2.AxiosRequestConfig<D>): Promise<any> {
-        try {
-            const res = await axios2.post(url, data, config)
-            return res
-        } catch(e: any) {
-            return e.response
-        }
-    },
-    get: async (...args: any) => {
-        try {
-            const res = await axios2.get(...args)
-            return res
-        } catch(e) {
-            return e.response
-        }
-    },
-    put: async (...args: any) => {
-        try {
-            const res = await axios2.put(...args)
-            return res
-        } catch(e) {
-            return e.response
-        }
-    },
-    delete: async (...args: any) => {
-        try {
-            const res = await axios2.delete(...args)
-            return res
-        } catch(e) {
-            return e.response
-        }
-    },
-}
+  post: async (...args: Parameters<typeof axios2.post>): Promise<any> => {
+    try {
+      const res = await axios2.post(...args);
+      return res;
+    } catch (e: any) {
+      return e.response;
+    }
+  },
+  get: async (...args: Parameters<typeof axios2.get>): Promise<any> => {
+    try {
+      const res = await axios2.get(...args);
+      return res;
+    } catch (e: any) {
+      return e.response;
+    }
+  },
+  put: async (...args: Parameters<typeof axios2.put>): Promise<any> => {
+    try {
+      const res = await axios2.put(...args);
+      return res;
+    } catch (e: any) {
+      return e.response;
+    }
+  },
+  delete: async (...args: Parameters<typeof axios2.delete>): Promise<any> => {
+    try {
+      const res = await axios2.delete(...args);
+      return res;
+    } catch (e: any) {
+      return e.response;
+    }
+  },
+};
